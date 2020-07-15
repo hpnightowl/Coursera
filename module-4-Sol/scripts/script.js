c1ebb0f7b501e6d698ab98ca70e916573bd6d1a3
HARSH <iamhp2k@gmail.com>
2020-07-15 17:06:30 +0530

    course: use std string in modue 4

--- module-4-Sol/scripts/script.js
+++ module-4-Sol/scripts/script.js
@@ -1,6 +1,6 @@
 (function (window) {
 
-var names = ["hpnightowl", "John", "Jonny", "Anonymous", "Yes", "jack", "Eating", "Sugar", "No", "Jack"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
 
   var firstLetter = ((names[i]).charAt(0)).toLowerCase();
