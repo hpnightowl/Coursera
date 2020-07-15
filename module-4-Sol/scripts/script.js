5e3bbb8846309332aae16e6f41f71fb02ef51654
hpnightowl <iamhp2k@gmail.com>
2020-07-15 14:49:32 +0530

    course: dummy assignment(without external libs)

--- module-4-Sol/scripts/script.js
+++ module-4-Sol/scripts/script.js
@@ -1,2 +1,15 @@
-var a = "Hello"
-console.log(a)
+(function (window) {
+
+var names = ["hpnightowl", "John", "Jonny", "", "Yes", "jack", "Eating", "Sugar", "No", "Jack"];
+
+for (var i in names) {
+
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+  if (firstLetter == 'J' || firstLetter == 'j') {
+       console.log("Hello" + " " + name);
+  } else {
+       console.log("Goodbye" + " " + name)
+  }
+}
+})(window);
